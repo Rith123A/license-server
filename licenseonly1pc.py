6a35a8e3b825dc0dc87f0adb74d1e5efa5b79a6d
Rith123A <ngimbunna23@gmail.com>
2025-03-05 04:16:07 -0800

    licenseonly1pc.py

--- licenseonly1pc.py
+++ licenseonly1pc.py
@@ -2,8 +2,8 @@
   "licenses": [
     {
       "key": "EXAMPLE-KEY-123456",
-      "pc_identifier": "0x1c6f65d4b8e2",
-      "expiration_date": "2024-12-31T23:59:59"
+      "pc_identifier": "0xaa1c04103638",
+      "expiration_date": "2025-12-31T23:59:59"
     }
     
   ]
